a58fb334986e36daff2730deb74a48f7ac064db6
Andrew Musselman <andrew.mussels@gmail.com>
2022-03-24 14:14:15 -0600

    Create All Movies component

---
+++ src/AllMovies.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const AllMovies = ({movies}) => {
+
+  const movieCards = movies.map(movie => {
+    return (
+      <MovieCard
+        image={movie.img}
+        title={movie.title}
+      )
+  })
+
+  return (
+    <div className='all-movie-container'>
+      {movieCards}
+    </div>
+  )
+}
+
+export default AllMovies;
