36dcf2436f285d2f71156b77728791bcae66eb92
Jessica Fatta <jessicafatta@gmail.com>
2022-03-24 18:09:25 -0400

    Add styling

---
+++ src/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Nav.css'
+import reel from './reel.png'
+
+const Nav = () => {
+  return (
+    <nav>
+      <img src={reel}/>
+      <h1>Rancid Tomatillos</h1>
+    </nav>
+  )
+}
+
+export default Nav
