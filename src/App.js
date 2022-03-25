e36ddeeb1a8747e6f93e9ecc8acd78d549ef4d09
Andrew Musselman <andrew.mussels@gmail.com>
2022-03-25 12:02:37 -0600

    Add method to show current movie details

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import './App.css';
+import aMovie from './dummy-movie-data';
 import movieData from './movie-data';
 import AllMovies from './AllMovies';
 import TopMovies from './TopMovies'
@@ -20,16 +21,19 @@ class App extends Component {
     this.setState({movies: movieData.movies})
   }
 
+  getMovieDetails = (id) => {
+    const currentMovie = this.state.movies.find(movie => movie.id === id)
+    this.setState({currentMovie: currentMovie})
+  }
+
   render() {
     return (
       <div>
         <Nav />
         <main className='App'>
-          {this.state.currentMovie ?
-          <CurrentMovie /> :
-          <TopMovies movies={this.state.movies}/>
-          <AllMovies movies={this.state.movies}/>
-        }
+          {this.state.currentMovie && <CurrentMovie currentMovie={this.state.currentMovie}/> }
+          <TopMovies movies={this.state.movies} getMovieDetails={this.getMovieDetails}/>
+          <AllMovies movies={this.state.movies} getMovieDetails={this.getMovieDetails}/>
         </main>
       </div>
     )
