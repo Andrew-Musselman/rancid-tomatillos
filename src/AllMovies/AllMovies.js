d2136da4e44e972ac7f2858af60cd37b8e29e7ac
Jessica Fatta <jessicafatta@gmail.com>
2022-03-25 20:22:50 -0400

    Create component file for all movies

---
+++ src/AllMovies/AllMovies.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import MovieCard from '../MovieCard/MovieCard';
+import './AllMovies.css'
+
+const AllMovies = ({movies, getMovieDetails}) => {
+
+  const movieCards = movies.map(movie => {
+    return (
+      <MovieCard
+        image={movie.poster_path}
+        title={movie.title}
+        getMovieDetails={getMovieDetails}
+        id={movie.id}
+        key={movie.id}
+        />
+      )
+  })
+
+  return (
+    <div className='all-movie-wrapper'>
+      <h2>All Movies</h2>
+      <div className='all-movie-container'>
+        {movieCards}
+      </div>
+    </div>
+  )
+}
+
+export default AllMovies;
