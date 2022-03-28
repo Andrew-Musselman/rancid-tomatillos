f1084cc3792a16be5e125a0ae0bfb3a13b4acf50
Jessica Fatta <jessicafatta@gmail.com>
2022-03-28 18:26:13 -0400

    Add new file path for new logo

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import './Nav.css'
 import reel from '../reel.png'
+import reel2 from '../reel2.png'
 
 const Nav = () => {
   return (
     <nav>
-      <img src={reel}/>
+      <img src={reel2}/>
       <h1>Rancid Tomatillos</h1>
     </nav>
   )
