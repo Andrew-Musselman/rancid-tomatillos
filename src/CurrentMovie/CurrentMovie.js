9963d68032fb6044435e80b2466d1f298920580c
Andrew Musselman <andrew.mussels@gmail.com>
2022-03-29 17:31:16 -0600

    Add Image div

--- src/CurrentMovie/CurrentMovie.js
+++ src/CurrentMovie/CurrentMovie.js
@@ -19,20 +19,23 @@ class CurrentMovie extends Component {
 
   render() {
     return (
-      <div>
+      <section className='current-movie-page'>
       {this.state.isLoading ? <h1>Loading...</h1> :
-      <div className='current-movie-details'>
-        <img src={this.state.currentMovie.backdrop_path} alt={`Still from ${this.state.currentMovie.title}`} />
-        <h2>{this.state.currentMovie.title}</h2>
-        <h3>{this.state.currentMovie.tagline}</h3>
-        <p>Released: {`${new Date(this.state.currentMovie.release_date).toLocaleDateString({year: 'numeric', month: '2-digit', day: '2-digit'})}`} | Run Time: {`${this.state.currentMovie.runtime} minutes`}</p>
-        <p>{this.state.currentMovie.overview}</p>
-        <p>{this.state.currentMovie.genre}</p>
-        <p>{`Average Rating: ${parseFloat(this.state.currentMovie.average_rating).toFixed(2)}`}</p>
-        <p>{`Budget: ${this.state.currentMovie.budget} | Box Office: ${this.state.currentMovie.revenue}`}</p>
-        <Link to='/'><button>Back</button></Link>
-      </div> }
-      </div>
+        <article className='current-movie-details' >
+          <div className='current-movie-image' style={{ backgroundImage: `linear-gradient(rgb(255 255 255 / 0%), rgb(179 176 176)), url(${this.state.currentMovie.backdrop_path})` }}>
+          </div>
+          <div className='current-movie-text'>
+            <h2>{this.state.currentMovie.title}</h2>
+            <h3>{this.state.currentMovie.tagline}</h3>
+            <p><b>Released:</b> {`${new Date(this.state.currentMovie.release_date).toLocaleDateString({year: 'numeric', month: '2-digit', day: '2-digit'})}`} | <b> Run Time: </b> {`${this.state.currentMovie.runtime} minutes`}</p>
+            <p>{this.state.currentMovie.overview}</p>
+            <p>{this.state.currentMovie.genre}</p>
+            <p><b>Average Rating:</b> {`${parseFloat(this.state.currentMovie.average_rating).toFixed(2)}`}</p>
+            <p><b>Budget:</b> {`${this.state.currentMovie.budget}`} | <b> Box Office: </b>{`${this.state.currentMovie.revenue}`}</p>
+            <Link to='/'><button>Back</button></Link>
+          </div>
+        </article> }
+      </section>
     )
   }
 }
