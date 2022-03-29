fce88fd27cc67130654f39c76a9d533f70093375
Andrew Musselman <andrew.mussels@gmail.com>
2022-03-29 15:50:33 -0600

    Add fetch call to CurrentMovie component

--- src/CurrentMovie/CurrentMovie.js
+++ src/CurrentMovie/CurrentMovie.js
@@ -1,22 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './CurrentMovie.css';
 import {Link} from 'react-router-dom';
+import { getSingleMovie } from '../apiCalls';
 
-const CurrentMovie = ({currentMovie, hideMovieDetails}) => {
+class CurrentMovie extends Component {
+  constructor() {
+    super()
+    this.state = {
+      currentMovie: '',
+      isLoading: true
+    }
+  }
 
-  return (
+  componentDidMount() {
+    getSingleMovie(this.props.currentMovieId)
+    .then(data => this.setState({currentMovie: data.movie, isLoading: false}))
+  }
+
+  render() {
+    return (
+      <div>
+      {this.state.isLoading ? <h1>Loading...</h1> :
       <div className='current-movie-details'>
-        <img src={currentMovie.backdrop_path} alt={`Still from ${currentMovie.title}`} />
-        <h2>{currentMovie.title}</h2>
-        <h3>{currentMovie.tagline}</h3>
-        <p>Released: {`${new Date(currentMovie.release_date).toLocaleDateString({year: 'numeric', month: '2-digit', day: '2-digit'})}`} | Run Time: {`${currentMovie.runtime} minutes`}</p>
-        <p>{currentMovie.overview}</p>
-        <p>{currentMovie.genre}</p>
-        <p>{`Average Rating: ${parseFloat(currentMovie.average_rating).toFixed(2)}`}</p>
-        <p>{`Budget: ${currentMovie.budget} | Box Office: ${currentMovie.revenue}`}</p>
+        <img src={this.state.currentMovie.backdrop_path} alt={`Still from ${this.state.currentMovie.title}`} />
+        <h2>{this.state.currentMovie.title}</h2>
+        <h3>{this.state.currentMovie.tagline}</h3>
+        <p>Released: {`${new Date(this.state.currentMovie.release_date).toLocaleDateString({year: 'numeric', month: '2-digit', day: '2-digit'})}`} | Run Time: {`${this.state.currentMovie.runtime} minutes`}</p>
+        <p>{this.state.currentMovie.overview}</p>
+        <p>{this.state.currentMovie.genre}</p>
+        <p>{`Average Rating: ${parseFloat(this.state.currentMovie.average_rating).toFixed(2)}`}</p>
+        <p>{`Budget: ${this.state.currentMovie.budget} | Box Office: ${this.state.currentMovie.revenue}`}</p>
         <Link to='/'><button>Back</button></Link>
+      </div> }
       </div>
-  )
+    )
+  }
 }
 
 export default CurrentMovie;
