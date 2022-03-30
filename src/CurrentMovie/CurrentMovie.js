6756d203a4f9b3b549fd41c534d0a5934aeba656
Jessica Fatta <jessicafatta@gmail.com>
2022-03-30 18:39:47 -0400

    Update fix from PR review

--- src/CurrentMovie/CurrentMovie.js
+++ src/CurrentMovie/CurrentMovie.js
@@ -7,15 +7,20 @@ class CurrentMovie extends Component {
   constructor() {
     super()
     this.state = {
-      currentMovie: '',
-      isLoading: true
+      currentMovie: null,
+      isLoading: true,
+      error: null
     }
   }
 
   componentDidMount() {
     getData(`movies/${this.props.currentMovieId}`)
     .then(data => this.setState({currentMovie: data.movie, isLoading: false}))
-    .catch(err => console.log(err))
+    .catch(err => this.setState({error: err}))
+  }
+
+  convertDate(releaseDate) {
+    return new Date(releaseDate).toLocaleDateString({year: 'numeric', month: '2-digit', day: '2-digit'})
   }
 
   render() {
@@ -28,7 +33,7 @@ class CurrentMovie extends Component {
           <div className='current-movie-text'>
             <h2>{this.state.currentMovie.title}</h2>
             <h3>{this.state.currentMovie.tagline}</h3>
-            <p><b>Released:</b> {`${new Date(this.state.currentMovie.release_date).toLocaleDateString({year: 'numeric', month: '2-digit', day: '2-digit'})}`} | <b> Run Time: </b> {`${this.state.currentMovie.runtime} minutes`}</p>
+            <p><b>Released:</b> {`${this.convertDate(this.state.currentMovie.release_date)}`} | <b> Run Time: </b> {`${this.state.currentMovie.runtime} minutes`}</p>
             <p>{this.state.currentMovie.overview}</p>
             <p>{this.state.currentMovie.genre}</p>
             <p><b>Average Rating:</b> {`${parseFloat(this.state.currentMovie.average_rating).toFixed(2)}`}</p>
