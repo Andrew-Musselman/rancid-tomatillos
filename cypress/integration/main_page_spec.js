4bb9b12e2067bf46cd2056252c1ac0977616b8ea
Jessica Fatta <jessicafatta@gmail.com>
2022-03-30 20:29:29 -0400

    Remove single movie describe block to put in own file

--- cypress/integration/main_page_spec.js
+++ cypress/integration/main_page_spec.js
@@ -15,29 +15,6 @@ const getAllMovieData = () => {
   }
 }
 
-const getSingleMovieData = () => {
-  return {
-    statusCode: 200,
-    ok: true,
-      body: {
-        movies: [{
-          title: 'Money Plane',
-          poster_path: 'https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg',
-          backdrop_path: 'https://image.tmdb.org/t/p/original//pq0JSpwyT2URytdFG0euztQPAyR.jpg',
-          release_date: '2020-09-29',
-          overview: 'A professional thief with $40 million in debt and his family\'s life on the line must commit one final heist - rob a futuristic airborne casino filled with the world\'s most dangerous criminals.',
-          genres: [
-            'Action'
-          ],
-          budget: 0,
-          revenue: 0,
-          runtime: 82,
-          tagline: '',
-          average_rating: 6.625
-        }]
-      }
-  }
-}
 
 
 describe('Main', () => {
@@ -81,10 +58,7 @@ describe('Main', () => {
     cy.visit('http://localhost:3000')
       .contains('Something went wrong!');
   })
-});
 
-
-describe('Movie Details', () => {
   it('As a user, I should be able to click on a movie card', () => {
     cy.intercept('GET', getAllMovieData())
     cy.visit('http://localhost:3000')
@@ -93,37 +67,5 @@ describe('Movie Details', () => {
       .click()
       .url().should('eq', 'http://localhost:3000/694919')
   })
+});
 
-  it('As a user, I should be able to see a movie title', () => {
-    cy.intercept('GET', getSingleMovieData())
-    cy.visit('http://localhost:3000/694919')
-    .contains('h2', 'Money Plane')
-  })
-
-  it('As a user, I should be able to see a movie image', () => {
-    cy.intercept('GET', getSingleMovieData())
-    cy.visit('http://localhost:3000/694919')
-    .get('img')
-  })
-
-  it('As a user, I should be able to see movie details', () => {
-    cy.intercept('GET', getSingleMovieData())
-    cy.visit('http://localhost:3000/694919')
-    .contains('p')
-  })
-
-  it('As a user, I should be able to click the back button to return to the home page', () => {
-    cy.intercept('GET', getSingleMovieData())
-    cy.visit('http://localhost:3000/694919')
-    .get('button')
-  })
-
-  it('As a user, I should be able to click the back button to return to the home page', () => {
-    cy.intercept('GET', getSingleMovieData())
-    cy.visit('http://localhost:3000/694919')
-    .get('button')
-    .click()
-    .url().should('eq', 'http://localhost:3000/')
-  })
-
-})
