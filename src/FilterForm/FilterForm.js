2f856318f431a241481838b6970004587c4fe2ee
Jessica Fatta <jessicafatta@gmail.com>
2022-04-01 19:18:59 -0400

    Add new form return

---
+++ src/FilterForm/FilterForm.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+
+const FilterForm = ({filterGenre}) => {
+
+  const handleChange = (genre) => {
+    filterGenre(genre)
+  }
+
+
+  return (
+    <div className="genre-filter">
+        <label className="filter-label" name="genre">Choose a genre:</label>
+          <select className="genre" name="genre" onChange={({target: {value}}) => handleChange(value)}>
+            <option value="All">All</option>
+            <option value="Action">Action</option>
+            <option value="Adventure">Adventure</option>
+            <option value="Animation">Animation</option>
+            <option value="Comedy">Comedy</option>
+            <option value="Crime">Crime</option>
+            <option value="Drama">Drama</option>
+            <option value="Family">Family</option>
+            <option value="Fantasy">Fantasy</option>
+            <option value="History">History</option>
+            <option value="Horror">Horror</option>
+            <option value="Music">Music</option>
+            <option value="Romance">Romance</option>
+            <option value="Science Fiction">Science Fiction</option>
+            <option value="Thriller">Thriller</option>
+            <option value="War">War</option>
+          </select>
+      </div>
+    )
+
+}
+
+
+
+
+export default FilterForm
