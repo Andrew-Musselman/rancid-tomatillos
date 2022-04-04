f94ea65976c607e5ac547a7335131ce6078189c9
Andrew Musselman <andrew.mussels@gmail.com>
2022-04-04 16:28:40 -0600

    Refactor MovieCard files

--- src/MovieCard/MovieCard.js
+++ src/MovieCard/MovieCard.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import './MovieCard.css';
 import { Link } from 'react-router-dom';
+import './MovieCard.css';
 
 const MovieCard = ({image, title, id, getMovieDetails}) =>{
   return (
     <div className='movie-card'>
       <Link to={`/${id}`} >
-        <img src={image} alt={`${title}'s poster`} className='movie-card-img'/>
+        <img src={image} alt={`${title}'s poster`} className='movie-card-img' />
       </Link>
     </div>
   )
